66d2beb884be867ef37fc3ddf92376f235fba33a
jahid <jahidmorol2@gmail.com>
2023-10-16 17:26:03 +0600

    some changes

--- components/nav-utils/Logo.jsx
+++ components/nav-utils/Logo.jsx
@@ -1,15 +1,14 @@
 import logo1 from "@/assets/logo.png";
+import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
   return (
     <Link href="/" className="flex flex-col md:flex-row gap-2 items-center">
-      <img
+      <Image
         className="w-20"
         src={logo1}
         alt="logo"
-        height="100"
-        width="100"
       />
       <div className=" hidden md:block text-center font-bold -space-y-1 text-lg">
         <p className="text-my-primary "> KBM Computer</p>{" "}
