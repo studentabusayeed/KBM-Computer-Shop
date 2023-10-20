67afe99cc10c632d8e44bc6536887b58a06c4268
nmcsakib <bksumon51@gmail.com>
2023-10-20 22:42:09 +0600

    fixed issue for colors and added 'add products'

--- components/CardDataStats.tsx
+++ components/CardDataStats.tsx
@@ -18,9 +18,9 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
   children,
 }) => {
   return (
-    <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default">
+    <div className="rounded-sm border border-stroke py-6 px-7.5 shadow-default">
       <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full
-       bg-meta-2 ">
+       bg-gray-500 ">
         {children}
       </div>
 
