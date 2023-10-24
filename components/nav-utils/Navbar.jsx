3eff4adacc6672bd287700596870555c7da21e3a
Imam Hossain <shadinfr@yahoo.com>
2023-10-24 21:47:40 +0600

    menu design change and button design changes

--- components/nav-utils/Navbar.jsx
+++ components/nav-utils/Navbar.jsx
@@ -9,27 +9,25 @@ const Navbar = () => {
 
 
   return (
-      <div
-        className={`w-full bg-white transition-all relative top-0 z-50 duration-700 ease-in-out`}
-      >
-        <div className="nav-container">
-          <div className="hidden xl:block">
-            <div className="flex items-center py-2 justify-between">
-                <Logo />
-                <Search />
-              
-              <div className="flex items-center gap-4 lg:gap-10">
-                <Language />
-                <NavWishlist/>
-                <NavCart />
-              <NavAvatar/>
-                
-              </div>
+    <div
+      className={`w-full bg-white transition-all relative top-0 z-50 duration-700 ease-in-out border-b border-gray-200`}
+    >
+      <div className="max-w-[1280px] w-full mx-auto px-3">
+        <div>
+          <div className="flex items-center py-2 justify-between">
+            <Logo />
+            <Search />
+
+            <div className="flex items-center gap-4 lg:gap-10">
+              <Language />
+              <NavWishlist />
+              <NavCart />
+              <NavAvatar />
             </div>
           </div>
         </div>
-        <hr className="border-gray-3" />
       </div>
+    </div>
   );
 };
 
