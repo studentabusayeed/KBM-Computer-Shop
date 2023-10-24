d33df4b40540bdcf8ab253d0a8fb1115f6dc12e4
Imam Hossain <shadinfr@yahoo.com>
2023-10-24 10:32:16 +0600

    price range added

--- components/SideBar/ProcessorModel.jsx
+++ components/SideBar/ProcessorModel.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 
 const ProcessorModel = () => {
     return (
-        <div className='flex flex-col gap-0.5 pb-3'>
-            <Checkbox size="sm" color="success" >Intel CDC</Checkbox>
-            <Checkbox size="sm" color="success" >Intel PQC</Checkbox>
-            <Checkbox size="sm" color="success" >Intel Core i3</Checkbox>
-            <Checkbox size="sm" color="success" >Intel Core i5</Checkbox>
-            <Checkbox size="sm" color="success" >Intel Core i7</Checkbox>
-            <Checkbox size="sm" color="success" >Intel Core i9</Checkbox>
-            <Checkbox size="sm" color="success">AMD Athlon</Checkbox>
-            <Checkbox size="sm" color="success">AMD Ryzen 3</Checkbox>
-            <Checkbox size="sm" color="success">AMD Ryzen 5</Checkbox>
-            <Checkbox size="sm" color="success">AMD Ryzen 7</Checkbox>
-            <Checkbox size="sm" color="success">AMD Ryzen 9</Checkbox>
-            <Checkbox size="sm" color="success">Apple M1</Checkbox>
+        <div className='flex flex-col gap-1 pb-3'>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Intel CDC</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Intel PQC</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Intel Core i3</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Intel Core i5</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Intel Core i7</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Intel Core i9</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>AMD Athlon</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>AMD Ryzen 3</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>AMD Ryzen 5</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>AMD Ryzen 7</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>AMD Ryzen 9</Checkbox>
+            <Checkbox size="sm" color="success" className='p-0 m-0'>Apple M1</Checkbox>
         </div>
     );
 };
