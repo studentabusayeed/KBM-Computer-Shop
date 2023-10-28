aaeefbeca684cb0c8c3285bb5311a106bd1acab3
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-10-28 14:44:52 +0600

    checkout page added

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -2,27 +2,10 @@ import HomeBanner from "@/app/(home)/HomeBanner";
 import SideBanner from "@/app/(home)/SideBanner";
 import React from "react";
 import CategorySection from "./CategorySection";
-import KBM_Title from "@/app/(home)/KBM_Title"
+import KbmTitle from "../../components/KbmTitle/KbmTitle";
+
 
 const HomePage = () => {
-<<<<<<< HEAD
-    return (
-        <>
-
-            <section className='w-full mb-16 grid grid-rows-1 gap-3 grid-cols-4  mt-auto'>
-                <aside>
-                    <SideBanner />
-                </aside>
-                <aside className='col-span-3'>
-                    <HomeBanner />
-                </aside>
-            </section>
-            <section>
-                <CategorySection />
-            </section>
-        </>
-    );
-=======
   return (
     <>
       <section className="w-full mb-16 grid grid-rows-1 gap-3 grid-cols-4  mt-auto">
@@ -37,11 +20,10 @@ const HomePage = () => {
         <CategorySection />
       </section>
       <section>
-        <KBM_Title />
+        <KbmTitle />
       </section>
     </>
   );
->>>>>>> e634c6f556a40207c056485b8172fb12ee5d00fb
 };
 
 export default HomePage;
